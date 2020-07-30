36db5a969f319e7d93866c0a21b4921916003e1f
Juliano Lopes <julianomoraeslopes@gmail.com>
2020-07-30 18:51:19 -0300

    Finalizado

--- js/index.js
+++ js/index.js
@@ -14,7 +14,55 @@ toggleButton.on("click", function(){
 });
 
 $("#gitHubSearchForm").on("submit", function () {
-    
+    var gitHubApiUrl = "https://api.github.com/search/repositories?q=";
+    var searchQuery = $("#searchQuery").val();
+    var useStars = $("#useStars").val();
+    var langChoice = $("#langChoice").val();
+
+    if (searchQuery) {
+        resultList.text("Performing search, please wait...");
+        gitHubApiUrl += encodeURIComponent(searchQuery);
+
+    if (langChoice != "All") {
+        gitHubApiUrl += "language:" +encodeURIComponent(langChoice);
+    }
+
+    if (useStars) {
+        gitHubApiUrl += "&sort=stars";
+    }
+    console.log(gitHubApiUrl);
+
+$.get(gitHubApiUrl)
+    .then(function successCallBack(r) {
+        resultList.empty();
+        displayResults(r.items);
+    },
+    function errorCallBack(r){
+        console.log("Failed to query GitHub");
+    });
+    } 
+    return false;
 });
 
-})
+function displayResults(results) {
+    resultList.empty
+    $.each(results, function(i, item) {
+        var newResult = $("<div class='result'>" +
+        "<div class='title'>" + item.name + "</div>" +
+        "<div> Language: " + item.language + "</div>" +
+        "<div> Owner: " + item.owner.login + "</div>" +
+        "</div>");
+    
+        newResult.hover(function () {
+            //make it darker
+            $(this).css("background-color", "lightgray");
+        }, function (){
+            //reverse
+            $(this).css("background-color", "transparent");
+        });
+    
+        resultList.append(newResult);
+    });
+    }
+
+});
